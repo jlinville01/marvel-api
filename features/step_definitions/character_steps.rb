766cd11575891b21f7d31cb5250fc525500f79c6
Justin Linville <justinlinville@Justins-iMac.local>
2019-09-18 11:03:18 -0700

    Removes test directory and refactors steps

--- features/step_definitions/character_steps.rb
+++ features/step_definitions/character_steps.rb
@@ -1,17 +1,17 @@
 Given(/^I send a valid GET request to get all characters$/) do
-	characters
+	@characters = characters
 end
 
 Then(/^all characters should return in the response$/) do
-
+	expect(@characters.count).to eql(10)
 end
 
 Given(/^I send a valid request for a specific character$/) do
-	specific_character(1)
+	@character = specific_character(1)
 end
 
 Then(/^a specific character should return in the response$/) do
-
+	expect(@character[:alias]).to eql('Captain America')
 end
 
 Given(/^I send a valid request to create character$/) do
@@ -19,7 +19,8 @@ Given(/^I send a valid request to create character$/) do
 end
 
 Then(/^a new character should be created$/) do
-
+	character = specific_character(11)
+	expect(character[:alias]).to eql(CHAR_ALIAS)
 end
 
 Given(/^I send a valid request to update character$/) do
@@ -27,13 +28,19 @@ Given(/^I send a valid request to update character$/) do
 end
 
 Then(/^the character should be updated$/) do
-
+	character = specific_character(1)
+	expect(character[:debut]).to eql('194777')
 end
 
 Given(/^I send a valid request to delete character$/) do
-	delete_character(1)
+	delete_character(3)
 end
 
 Then(/^the character should be deleted$/) do
-
+	begin
+		specific_character(3)
+	rescue e
+		puts e.response.code
+		# expect(e.response.code).to eql()
+	end
 end
--- features/step_definitions/lib/character_methods.rb
+++ features/step_definitions/lib/character_methods.rb
@@ -1,23 +1,24 @@
 def characters
 	res = RestClient.get("#{BASE_HOST}/characters", nil)
 	parsed_res = JSON.parse(res, symbolize_names: true)
+	puts parsed_res[:data].to_yaml
 	expect(res.code).to eql(200)
 	expect(parsed_res[:status]).to eql('SUCCESS')
 	expect(parsed_res[:message]).to eql('Loaded characters')
+	parsed_res[:data]
 rescue RuntimeError => e
-	puts e.response.code
 	puts e.response
 end
 
 def specific_character(id)
-	res = RestClient.get("#{BASE_HOST}/characters/1", nil)
+	res = RestClient.get("#{BASE_HOST}/characters/#{id}", nil)
 	parsed_res = JSON.parse(res, symbolize_names: true)
 	expect(res.code).to eql(200)
 	expect(parsed_res[:status]).to eql('SUCCESS')
 	expect(parsed_res[:message]).to eql('Loaded character')
-	expect(parsed_res[:data][:id]).to eql(1)
+	expect(parsed_res[:data][:id]).to eql(id)
+	parsed_res[:data]
 rescue RuntimeError => e
-	puts e.response.code
 	puts e.response
 end
 
@@ -37,7 +38,6 @@ def create_character(char_alias, real_name, debut, status, powers, bio)
 	expect(parsed_res[:message]).to eql('Saved character')
 	expect(parsed_res[:data][:alias]).to eql(char_alias)
 rescue RuntimeError => e
-	puts e.response.code
 	puts e.response
 end
 
@@ -53,18 +53,15 @@ def update_character(id)
 	expect(parsed_res[:message]).to eql('Updated character')
 	expect(parsed_res[:data][:debut]).to eql(new_debut)
 rescue RuntimeError => e
-	puts e.response.code
 	puts e.response
 end
 
 def delete_character(id)
 	res = RestClient.delete("#{BASE_HOST}/characters/#{id}", nil)
 	parsed_res = JSON.parse(res, symbolize_names: true)
-	puts parsed_res.to_yaml
 	expect(res.code).to eql(200)
 	expect(parsed_res[:status]).to eql('SUCCESS')
 	expect(parsed_res[:message]).to eql('Deleted character')
 rescue RuntimeError => e
-	puts e.response.code
 	puts e.response
 end
